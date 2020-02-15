7aa616ff6aaf5691503ec4ae5dac760fd3b272dc
Julien Doutre <jul.doutre@gmail.com>
2020-02-15 23:45:03 +0100

    Add a basic CLI logic

--- beagle/__main__.py
+++ beagle/__main__.py
@@ -2,12 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import argparse
 from beagle.logging import init_logger
 
 
 def main() -> None:
     init_logger()
-    logging.info("Hello world")
+
+    parser = argparse.ArgumentParser(
+        prog="beagle",
+        description="A text search-engine over the Stanford CS276 document collection.",
+    )
+    parser.add_argument("-v", "--verbose", help="verbose mode", action="store_true")
+
+    subparsers = parser.add_subparsers(help="available commands", dest="cmd")
+    index_parser = subparsers.add_parser("index", help="to index the collection")
+    search_parser = subparsers.add_parser("search", help="to query the collection")
+
+    args = parser.parse_args()
+    if args.cmd == "index":
+        pass
+    elif args.cmd == "search":
+        pass
+    else:
+        raise parser.error(f"invalid command {args.cmd}")
 
 
 if __name__ == "__main__":
