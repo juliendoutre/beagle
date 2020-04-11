22c6a277a84307d867e7276dfef6dce47b45859e
Julien Doutre <jul.doutre@gmail.com>
2020-04-11 15:34:22 +0200

    Add fixtures test

--- tests/indexing_test.py
+++ tests/indexing_test.py
@@ -3,6 +3,26 @@ from beagle.index import InvertedIndex, InvertedIndexType
 from beagle.collection import Shard, Document
 
 
+@pytest.fixture()
+def shard():
+    doc0 = Document("", "", 0)
+    doc0.tokens = ["0", "1", "2"]
+
+    doc1 = Document("", "", 1)
+    doc1.tokens = ["3", "4", "2", "2", "2"]
+
+    doc2 = Document("", "", 2)
+    doc2.tokens = ["5"]
+
+    doc3 = Document("", "", 3)
+    doc3.tokens = ["3", "3"]
+
+    s = Shard("", "")
+    s.documents = [doc0, doc1, doc2, doc3]
+
+    return s
+
+
 class TestIndexUpdate:
     def test_both_empty_index(self):
         idx = InvertedIndex(InvertedIndexType.DOCUMENTS_INDEX)
@@ -33,23 +53,8 @@ class TestIndexUpdate:
 
 
 class TestIndexing:
-    def test_document_index(self):
-        doc0 = Document("", "", 0)
-        doc0.tokens = ["0", "1", "2"]
-
-        doc1 = Document("", "", 1)
-        doc1.tokens = ["3", "4", "2", "2", "2"]
-
-        doc2 = Document("", "", 2)
-        doc2.tokens = ["5"]
-
-        doc3 = Document("", "", 3)
-        doc3.tokens = ["3", "3"]
-
-        s = Shard("", "")
-        s.documents = [doc0, doc1, doc2, doc3]
-
-        assert s.index(InvertedIndexType.DOCUMENTS_INDEX).entries == {
+    def test_document_index(self, shard):
+        assert shard.index(InvertedIndexType.DOCUMENTS_INDEX).entries == {
             "0": [1, [0]],
             "1": [1, [0]],
             "2": [2, [0, 1]],
@@ -58,23 +63,8 @@ class TestIndexing:
             "5": [1, [2]],
         }
 
-    def test_frequencies_index(self):
-        doc0 = Document("", "", 0)
-        doc0.tokens = ["0", "1", "2"]
-
-        doc1 = Document("", "", 1)
-        doc1.tokens = ["3", "4", "2", "2", "2"]
-
-        doc2 = Document("", "", 2)
-        doc2.tokens = ["5"]
-
-        doc3 = Document("", "", 3)
-        doc3.tokens = ["3", "3"]
-
-        s = Shard("", "")
-        s.documents = [doc0, doc1, doc2, doc3]
-
-        assert s.index(InvertedIndexType.FREQUENCIES_INDEX).entries == {
+    def test_frequencies_index(self, shard):
+        assert shard.index(InvertedIndexType.FREQUENCIES_INDEX).entries == {
             "0": [1, [(0, 1)]],
             "1": [1, [(0, 1)]],
             "2": [2, [(0, 1), (1, 3)]],
@@ -83,23 +73,8 @@ class TestIndexing:
             "5": [1, [(2, 1)]],
         }
 
-    def test_positions_index(self):
-        doc0 = Document("", "", 0)
-        doc0.tokens = ["0", "1", "2"]
-
-        doc1 = Document("", "", 1)
-        doc1.tokens = ["3", "4", "2", "2", "2"]
-
-        doc2 = Document("", "", 2)
-        doc2.tokens = ["5"]
-
-        doc3 = Document("", "", 3)
-        doc3.tokens = ["3", "3"]
-
-        s = Shard("", "")
-        s.documents = [doc0, doc1, doc2, doc3]
-
-        assert s.index(InvertedIndexType.POSITIONS_INDEX).entries == {
+    def test_positions_index(self, shard):
+        assert shard.index(InvertedIndexType.POSITIONS_INDEX).entries == {
             "0": [1, [(0, 1, [0])]],
             "1": [1, [(0, 1, [1])]],
             "2": [2, [(0, 1, [2]), (1, 3, [2, 3, 4])]],
